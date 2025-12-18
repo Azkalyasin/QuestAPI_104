8229fbff6aa0c6f07720e3589ab52e0753b7b6c6
Azkalyasin <yasinadzkiaazkal@gmail.com>
2025-12-18 16:36:47 +0700

    menambhkan serivice api siswa get

---
+++ app/src/main/java/com/example/questapi_104/apiservice/ServiceApiSiswa.kt
@@ -0,0 +1,15 @@
+package com.example.questapi_104.apiservice
+
+import com.example.questapi_104.modeldata.DataSiswa
+import retrofit2.http.Body
+import retrofit2.http.DELETE
+import retrofit2.http.GET
+import retrofit2.http.POST
+import retrofit2.http.PUT
+import retrofit2.http.Query
+
+interface ServiceApiSiswa {
+    @GET("bacaTeman.php")
+    suspend fun getSiswa(): List<DataSiswa>
+
+}

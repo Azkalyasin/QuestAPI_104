2202df2b5daf75eb6826ce84b482bb5b8d6a9b4d
Azkalyasin <yasinadzkiaazkal@gmail.com>
2025-12-18 16:36:58 +0700

    menambhkan serivice api siswa post

---
+++ app/src/main/java/com/example/questapi_104/repositori/RepositoriDataSiswa.kt
@@ -0,0 +1,19 @@
+package com.example.questapi_104.repositori
+
+import com.example.questapi_104.apiservice.ServiceApiSiswa
+import com.example.questapi_104.modeldata.DataSiswa
+
+
+interface RepositoryDataSiswa {
+    suspend fun getDataSiswa(): List<DataSiswa>
+    suspend fun postDataSiswa(dataSiswa: DataSiswa):retrofit2.Response<Void>
+}
+
+class jaringanRepositoryDataSiswa (
+    private val serviceApiSiswa: ServiceApiSiswa
+): RepositoryDataSiswa{
+    override suspend fun getDataSiswa(): List<DataSiswa> = serviceApiSiswa.getSiswa()
+    override suspend fun postDataSiswa(dataSiswa: DataSiswa):retrofit2
+        .Response<Void> = serviceApiSiswa.postSiswa(dataSiswa)
+}
+

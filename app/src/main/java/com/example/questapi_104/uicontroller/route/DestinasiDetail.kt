c6258d9f816862f417f227e8052ca4018e9c706a
Azkalyasin <yasinadzkiaazkal@gmail.com>
2025-12-25 22:04:31 +0700

    penmbahan destinasi detail

--- app/src/main/java/com/example/questapi_104/uicontroller/route/DestinasiDetail.kt
+++ app/src/main/java/com/example/questapi_104/uicontroller/route/DestinasiDetail.kt
@@ -5,4 +5,6 @@ import com.example.questapi_104.R
 object DestinasiDetail : DestinasiNavigasi {
     override val route = "item_detail"
     override val titleRes = R.string.detail_siswa
+    const val itemIdArg = "idsiswa"
+    val routeWithArgs = "$route/{$itemIdArg}"
 }

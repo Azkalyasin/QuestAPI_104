77e10ad648b6218c2f46f904d87f03316a9ef798
Azkalyasin <yasinadzkiaazkal@gmail.com>
2025-12-25 22:10:53 +0700

    penmabhan getsatusiswa pada interface repositorysiswa

--- app/src/main/java/com/example/questapi_104/repositori/RepositoriDataSiswa.kt
+++ app/src/main/java/com/example/questapi_104/repositori/RepositoriDataSiswa.kt
@@ -7,13 +7,15 @@ import com.example.questapi_104.modeldata.DataSiswa
 interface RepositoryDataSiswa {
     suspend fun getDataSiswa(): List<DataSiswa>
     suspend fun postDataSiswa(dataSiswa: DataSiswa):retrofit2.Response<Void>
+    suspend fun getSatuSiswa(id: Int) : DataSiswa
 }
 
 class jaringanRepositoryDataSiswa (
     private val serviceApiSiswa: ServiceApiSiswa
 ): RepositoryDataSiswa{
-    override suspend fun getDataSiswa(): List<DataSiswa> = serviceApiSiswa.getSiswa()
-    override suspend fun postDataSiswa(dataSiswa: DataSiswa):retrofit2
-        .Response<Void> = serviceApiSiswa.postSiswa(dataSiswa)
+    override suspend fun getDataSiswa() : List<DataSiswa> = serviceApiSiswa.getSiswa()
+    override suspend fun postDataSiswa(dataSiswa: DataSiswa):retrofit2.Response<Void> =
+        serviceApiSiswa.postSiswa(dataSiswa)
+    override suspend fun getSatuSiswa(id: Int): DataSiswa = serviceApiSiswa.getSatuSiswa(id)
 }
 

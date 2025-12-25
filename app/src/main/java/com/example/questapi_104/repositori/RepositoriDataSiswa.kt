704e55cd500d018821b8be519c67452d077da289
Azkalyasin <yasinadzkiaazkal@gmail.com>
2025-12-25 22:11:30 +0700

    penambahan editsatusiswa pada interface repositorysiswa

--- app/src/main/java/com/example/questapi_104/repositori/RepositoriDataSiswa.kt
+++ app/src/main/java/com/example/questapi_104/repositori/RepositoriDataSiswa.kt
@@ -8,6 +8,7 @@ interface RepositoryDataSiswa {
     suspend fun getDataSiswa(): List<DataSiswa>
     suspend fun postDataSiswa(dataSiswa: DataSiswa):retrofit2.Response<Void>
     suspend fun getSatuSiswa(id: Int) : DataSiswa
+    suspend fun editSatuSiswa(id: Int, dataSiswa: DataSiswa): retrofit2.Response<Void>
 }
 
 class jaringanRepositoryDataSiswa (
@@ -17,5 +18,7 @@ class jaringanRepositoryDataSiswa (
     override suspend fun postDataSiswa(dataSiswa: DataSiswa):retrofit2.Response<Void> =
         serviceApiSiswa.postSiswa(dataSiswa)
     override suspend fun getSatuSiswa(id: Int): DataSiswa = serviceApiSiswa.getSatuSiswa(id)
+    override suspend fun editSatuSiswa(id: Int, dataSiswa: DataSiswa):retrofit2.Response<Void> =
+        serviceApiSiswa.editSatuSiswa(id, dataSiswa)
 }
 
